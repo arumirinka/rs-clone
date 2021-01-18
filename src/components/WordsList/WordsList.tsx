743b90391b1325150c2545faec4979bfcd2c0b5c
Dmitry Gutkovsky <femiarkh@gmail.com>
2021-01-18 09:54:51 +0300

    feat: add words list layout

---
+++ src/components/WordsList/WordsList.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { PlayCircleOutlined } from '@ant-design/icons';
+import { Button } from 'antd';
+import content from '../../content.json';
+import './WordsList.css';
+import voiceLanguage from './voiceLanguage';
+import { wordsListHeader, practiseButtonText } from './wordListTranslate';
+
+const WordsList: React.FC = () => {
+  interface Lesson {
+    UI:string;
+    learning:string;
+    level:number;
+    lesson:number;
+  }
+
+  const current:Lesson = {
+    UI: 'russian',
+    learning: 'english',
+    level: 1,
+    lesson: 1,
+  };
+
+  const theContent:any = content;
+  const { words } = theContent[current.UI][current.learning][`level${current.level}`][`lesson${current.lesson}`];
+
+  function handleWordClick(word: string) {
+    const utter = new SpeechSynthesisUtterance();
+    utter.lang = voiceLanguage[current.learning];
+    utter.text = word;
+    window.speechSynthesis.speak(utter);
+  }
+
+  return (
+    <div className="words">
+      <h2 className="words__header">{wordsListHeader[0]}</h2>
+      <table className="words__table">
+        <thead />
+        <tbody>
+          {words.map((word:string[]) => (
+            <tr key={word.toString()}>
+              <td>
+                <div>{word[0]}</div>
+              </td>
+              <td>
+                <button className="words__play-button" type="button" onClick={() => handleWordClick(word[0])}>
+                  <PlayCircleOutlined />
+                </button>
+              </td>
+              <td>
+                <div>{word[1]}</div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <Button type="primary">
+        {practiseButtonText[0]}
+      </Button>
+    </div>
+  );
+};
+
+export default WordsList;
---
+++ src/components/WordsList/wordListTranslate.ts
@@ -0,0 +1,10 @@
+export const wordsListHeader: string [] = [
+  'Слова к уроку',
+  'Words for the Lesson',
+  'Worte für die Lektion',
+];
+export const practiseButtonText: string [] = [
+  'К упражнениям',
+  'Go practise',
+  'Üben',
+];
