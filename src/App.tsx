5b8b61be09870f079965101a0d08f2d6b5ffe5b8
femiarkh <55946100+femiarkh@users.noreply.github.com>
2021-01-19 09:34:56 +0300

    feat: add word list page (#6)
    * feat: add words list layout
    
    * feat: add adaptive styles
    
    * feat: add route to words list
    
    * fix: bind header and button text to current UI lang
    
    * fix: highlight play sound icons on hover

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Main from './pages/Main/Main';
 import StatsPage from './pages/StatsPage/StatsPage';
 import Footer from './components/Footer/Footer';
 import StepsLayout from './components/StepsLayout/StepsLayout';
+import WordsList from './components/WordsList/WordsList';
 import LessonsLayout from './components/LessonsLayout/LessonsLayout';
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
           <Route path="/" component={Main} exact />
           <Route path="/stats" component={StatsPage} exact />
           <Route path="/steps" component={StepsLayout} exact />
+          <Route path="/words" component={WordsList} exact />
           <Route path="/lessons" component={LessonsLayout} exact />
         </Switch>
       </div>
