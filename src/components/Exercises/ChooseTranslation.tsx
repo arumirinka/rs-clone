3b8eaac41961e2b3664ee288e5a9812d8e170c76
Ekaterina <fedotova490@gmail.com>
2021-01-19 21:10:25 +0300

    fix: fix re-rendering of chooseTranslation buttons

--- src/components/Exercises/ChooseTranslation.tsx
+++ src/components/Exercises/ChooseTranslation.tsx
@@ -4,6 +4,12 @@ import content from '../../content.json';
 import ChooseTranslationBtn from './ChooseTranslationBtn';
 import './chooseTranslation.css';
 
+const getRandomIndexes = ():number[] => {
+  const arr:number[] = [0, 1, 2, 3];
+  return arr.sort(() => 0.5 - Math.random());
+};
+let array = getRandomIndexes();
+
 const chooseTranslation = () => {
   const theContent:any = content;
   interface Lesson {
@@ -20,9 +26,10 @@ const chooseTranslation = () => {
   };
   const { words } = theContent[current.UI][current.learning][`level${current.level}`][`lesson${current.lesson}`];
 
-  const randomWords:string[][] = words
+  const getRandomWords = ():string[][] => words
     .sort(() => 0.5 - Math.random())
     .slice(0, 4);
+  const randomWords = getRandomWords;
   const [wordsArray, setWordsArray] = useState(randomWords);
   const wordToCheck:string = wordsArray[0][0];
   const translationToCheck:string = wordsArray[0][1];
@@ -32,10 +39,10 @@ const chooseTranslation = () => {
       .sort(() => 0.5 - Math.random())
       .slice(0, 4);
     setWordsArray(newWords);
+    array = getRandomIndexes();
   };
   const [progress, setProgress] = useState(0);
-  const arr:number[] = [0, 1, 2, 3];
-  arr.sort(() => 0.5 - Math.random());
+  console.log(array);
   return (
     <div className="chooseTranslation-container">
       <div className="exercises-container__progress-bar">
@@ -44,25 +51,25 @@ const chooseTranslation = () => {
       <div className="chooseTranslation-container__word">Выберите перевод для слова &quot;{wordToCheck}&quot;</div>
       <div className="chooseTranslation-container__buttons">
         <ChooseTranslationBtn
-          translation={wordsArray[arr[0]][1]}
+          translation={wordsArray[array[0]][1]}
           translationToCheck={translationToCheck}
           currentProgress={progress}
           updateProgress={setProgress}
         />
         <ChooseTranslationBtn
-          translation={wordsArray[arr[1]][1]}
+          translation={wordsArray[array[1]][1]}
           translationToCheck={translationToCheck}
           currentProgress={progress}
           updateProgress={setProgress}
         />
         <ChooseTranslationBtn
-          translation={wordsArray[arr[2]][1]}
+          translation={wordsArray[array[2]][1]}
           translationToCheck={translationToCheck}
           currentProgress={progress}
           updateProgress={setProgress}
         />
         <ChooseTranslationBtn
-          translation={wordsArray[arr[3]][1]}
+          translation={wordsArray[array[3]][1]}
           translationToCheck={translationToCheck}
           currentProgress={progress}
           updateProgress={setProgress}
