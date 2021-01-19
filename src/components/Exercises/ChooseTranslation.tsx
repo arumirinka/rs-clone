9328b47b09f27a6d7e67d0d86f1718c5463ed005
Ekaterina <fedotova490@gmail.com>
2021-01-19 09:46:46 +0300

    feat: add change of words on click

--- src/components/Exercises/ChooseTranslation.tsx
+++ src/components/Exercises/ChooseTranslation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'antd';
 import content from '../../content.json';
 import ChooseTranslationBtn from './ChooseTranslationBtn';
@@ -20,38 +20,48 @@ const chooseTranslation = () => {
   };
   const { words } = theContent[current.UI][current.learning][`level${current.level}`][`lesson${current.lesson}`];
 
-  const createRandomWords = ():string[] => words
+  const randomWords:string[][] = words
     .sort(() => 0.5 - Math.random())
     .slice(0, 4);
-  const randomWords = createRandomWords();
-  const wordToCheck:string = randomWords[0][0];
-  const translationToCheck:string = randomWords[0][1];
+  //   const m = randomWords;
+  console.log(randomWords);
+  //   const randomWords = createRandomWords();
+  const [wordsArray, setWordsArray] = useState(randomWords);
+  const wordToCheck:string = wordsArray[0][0];
+  const translationToCheck:string = wordsArray[0][1];
+
+  const showNewWords = ():void => {
+    const newWords = words
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 4);
+    setWordsArray(newWords);
+    //   console.log(wordsArray);
+  };
 
   const arr:number[] = [0, 1, 2, 3];
   arr.sort(() => 0.5 - Math.random());
-
   return (
     <div className="chooseTranslation-container">
       <div className="chooseTranslation-container__word">Выберите перевод для слова &quot;{wordToCheck}&quot;</div>
       <div className="chooseTranslation-container__buttons">
         <ChooseTranslationBtn
-          translation={randomWords[arr[0]][1]}
+          translation={wordsArray[arr[0]][1]}
           translationToCheck={translationToCheck}
         />
         <ChooseTranslationBtn
-          translation={randomWords[arr[1]][1]}
+          translation={wordsArray[arr[1]][1]}
           translationToCheck={translationToCheck}
         />
         <ChooseTranslationBtn
-          translation={randomWords[arr[2]][1]}
+          translation={wordsArray[arr[2]][1]}
           translationToCheck={translationToCheck}
         />
         <ChooseTranslationBtn
-          translation={randomWords[arr[3]][1]}
+          translation={wordsArray[arr[3]][1]}
           translationToCheck={translationToCheck}
         />
       </div>
-      <Button type="primary" htmlType="submit">Продолжить</Button>
+      <Button type="primary" htmlType="submit" onClick={() => { showNewWords(); }}>Продолжить</Button>
     </div>
   );
 };
