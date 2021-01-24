3c06c2bed43b011fb6c8a6f74767a4259d29f8fe
Irina Stepanova <irina.s.vesna@mail.ru>
2021-01-24 06:12:30 +0600

    feat: add store, reducers, actions

---
+++ src/redux/appReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  appLang: 'russian',
+};
+
+export default function appReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'CHANGE_APP_LANG': {
+      const lang = action.payload;
+      return {
+        ...state,
+        appLang: lang,
+      };
+    }
+    default:
+      return state;
+  }
+}
