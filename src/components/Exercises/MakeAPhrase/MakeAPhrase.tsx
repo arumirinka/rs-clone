ef9b81c9a15a7d80271be038f94235225afaed17
Ekaterina <fedotova490@gmail.com>
2021-01-26 13:55:09 +0300

    fix: fix appending and removing of words on click

--- src/components/Exercises/MakeAPhrase/MakeAPhrase.tsx
+++ src/components/Exercises/MakeAPhrase/MakeAPhrase.tsx
@@ -18,34 +18,40 @@ const MakeAPhrase = ({ randomPhrases, progress, setProgress }:Props) => {
     .filter((newEl, index) => index === 1))
     .flat()
     .flat();
-  console.log(setPoints);
   const phraseToCheck:any = phrasesArray[0][0];
   const translationToCheck:string[] = phrasesArray[0][1].replace(/[.,!?]+/g, '').split(' ');
 
-  const [btnStyle, setBtnStyle] = useState(false);
   const buttonsContainer = useRef<HTMLDivElement>(null!);
   const [continueBtnDisabled, setContinueBtnDisabled] = useState(true);
   const [visible, setVisible]: any[] = useState(false);
+  interface IWords{
+    type:string
+    className:string
+    key:string
+    dataset:any
+    onClick:any
+  }
+  const [phraseArray, setPhraseArray] = useState<string[]>([]);
+  const generateRandomButtons = (x:any) => x.sort(() => Math.random() - 0.5);
+  const [wordsBtns, setWordsBtns] = useState(generateRandomButtons(translationPerWordArray
+    .slice(0, translationToCheck.length + 3)));
+  const [words, setWords] = useState<IWords[]>([]);
   const showModal = (): void => {
     setVisible(true);
   };
   const showNewWords = ():void => {
     if (progress === 100) {
-      setBtnStyle(true);
       showModal();
     } else {
-      const buttons = Array.from(buttonsContainer.current.children);
-      buttons.forEach((button:any) => {
-        button.classList.remove('buttons__translateBtn--correct', 'buttons__translateBtn--wrong', 'buttons__translateBtn--bigger');
-      });
       const newPhrases = phrasesArray
         .slice(1, 10);
       newPhrases.push(phrasesArray[0]);
       console.log(newPhrases);
       setPhrasesArray(newPhrases);
       setContinueBtnDisabled(true);
-      setBtnStyle(false);
       setProgress(progress + 10);
+      setPoints(points + 1);
+      setWords([]);
     }
   };
   window.addEventListener('keypress', (event) => {
@@ -54,43 +60,57 @@ const MakeAPhrase = ({ randomPhrases, progress, setProgress }:Props) => {
     }
   });
   const phraseMakerContainer = useRef<HTMLDivElement>(null!);
-  const phraseArray:string[] = [];
+
   const handleClick = (event:any) => {
-    const phraseMakerContainerChildren = Array.from(phraseMakerContainer.current.children);
-    if (phraseMakerContainerChildren
-      .find((el:any) => el.dataset.id === event.target.dataset.id) !== undefined) {
-      phraseMakerContainer.current.removeChild(event.target);
-      buttonsContainer.current.appendChild(event.target);
-      const wordToRemoveIndex = phraseArray.indexOf(event.target.dataset.id);
+    const newWord = event.target;
+    if (words.find((word) => word.dataset.id === newWord.dataset.id
+    && !Array.from(buttonsContainer.current.children).includes(event.target))) {
+      setWords((prev) => prev.filter((word) => word.dataset.id !== newWord.dataset.id));
+      setWordsBtns((prev:string[]) => [...prev, newWord.dataset.id]);
+      const wordToRemoveIndex = phraseArray.indexOf(newWord.dataset.id);
       phraseArray.splice(wordToRemoveIndex, 1);
     } else {
-      phraseMakerContainer.current.appendChild(event.target);
-      phraseArray.push(event.target.dataset.id);
+      setWords((prev) => [...prev, newWord]);
+      setPhraseArray((prev) => [...prev, newWord.dataset.id]);
+      setWordsBtns((prev:string[]) => prev.filter((word) => word !== newWord.dataset.id));
     }
+    console.log(words);
     console.log(phraseArray);
+    // if (phraseMakerContainer.current.children.length >= 0) {
+    setContinueBtnDisabled(false);
+    // } else { setContinueBtnDisabled(true); }
   };
-  const generateRandomButtons = (x:any) => x.sort(() => Math.random() - 0.5);
   const generateKey = (index:string) => `${index}_${new Date().getMilliseconds()}`;
   return (
     <>
       <div className="makeAPhrase-container__phrase">Составьте фразу: &quot;{phraseToCheck}&quot;</div>
       <div className="makeAPhrase-container__phrase-maker">
-        <div className="phrase-maker__inner-container" ref={phraseMakerContainer} />
-      </div>
-      <div className="makeAPhrase__buttons" ref={buttonsContainer}>
-        {generateRandomButtons(translationPerWordArray.slice(0, translationToCheck.length + 3)
-          .map((el:any, index:number) => (
+        <div className="phrase-maker__inner-container" ref={phraseMakerContainer}>
+          {words.map((word, index) => (
             <button
               type="button"
               className="buttons__makeAPhraseBtn"
-              key={generateKey(index.toString())}
-              data-id={el}
+              key={generateKey(String(index))}
+              data-id={word.dataset.id}
               onClick={(event) => handleClick(event)}
-              disabled={btnStyle}
             >
-              {el}
+              {word.dataset.id}
             </button>
-          )))}
+          ))}
+        </div>
+      </div>
+      <div className="makeAPhrase__buttons" ref={buttonsContainer}>
+        {wordsBtns.map((el:any, index:number) => (
+          <button
+            type="button"
+            className="buttons__makeAPhraseBtn"
+            key={generateKey(index.toString())}
+            data-id={el}
+            onClick={(event) => handleClick(event)}
+          >
+            {el}
+          </button>
+        ))}
       </div>
       <EndOfExerciseModal
         visible={visible}
@@ -102,7 +122,7 @@ const MakeAPhrase = ({ randomPhrases, progress, setProgress }:Props) => {
         onClick={() => { showNewWords(); }}
         className="chooseTranslation-container__continueButton"
         disabled={continueBtnDisabled}
-      >Продолжить
+      >Проверить
       </Button>
     </>
   );
