b5756472ee7ef9a75caa6292e7bba22cf5570b66
Irina Stepanova <irina.s.vesna@mail.ru>
2021-01-27 19:49:41 +0600

    feat: add langs from store to exercises-layout

--- src/components/Exercises/ExercisesLayout.tsx
+++ src/components/Exercises/ExercisesLayout.tsx
@@ -1,33 +1,46 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import ChooseTranslation from './ChooseTranslation';
 import content from '../../content.json';
 import KittyWithPencil from './KittyWithPencil';
 
-const theContent:any = content;
-interface Lesson {
-  UI:string;
-  learning:string;
-  level:number;
-  lesson:number;
-}
-const current:Lesson = {
-  UI: 'russian',
-  learning: 'english',
-  level: 1,
-  lesson: 1,
-};
-const { words } = theContent[current.UI][current.learning][`level${current.level}`][`lesson${current.lesson}`];
-// to increase number of random words - slice the array
-const randomWords:string[][] = words
-  .sort(() => 0.5 - Math.random())
-  .slice(0, 10);
+const getRandomWords = (appState: { appLang: string; learnLang: string; }) => {
+  const theContent:any = content;
+  interface Lesson {
+    UI:string;
+    learning:string;
+    level:number;
+    lesson:number;
+  }
+  const current:Lesson = {
+    UI: appState.appLang,
+    learning: appState.learnLang,
+    level: 1,
+    lesson: 1,
+  };
+  const { words } = theContent[current.UI][current.learning][`level${current.level}`][`lesson${current.lesson}`];
+  // to increase number of random words - slice the array
+  const randomWords:string[][] = words
+    .sort(() => 0.5 - Math.random())
+    .slice(0, 10);
 
-const ExercisesLayout: React.FC = () => (
-  <div className="exercises-container">
-    <div className="exercises-container__kitty">
-      <KittyWithPencil />
+  return randomWords;
+};
+
+const ExercisesLayout: React.FC = () => {
+  const selectAppState = (state: { app: any; }) => state.app;
+  const appState = useSelector(selectAppState);
+
+  const randomWords = getRandomWords(appState);
+
+  return (
+    <div className="exercises-container">
+      <div className="exercises-container__kitty">
+        <KittyWithPencil />
+      </div>
+      <ChooseTranslation randomWords={randomWords} />
     </div>
-    <ChooseTranslation randomWords={randomWords} />
-  </div>
-);
+  );
+};
+
 export default ExercisesLayout;
