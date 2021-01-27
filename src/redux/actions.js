e84110370008b1c0e302a23a6342bf59f84d73e6
Irina Stepanova <irina.s.vesna@mail.ru>
2021-01-27 16:59:51 +0600

    feat: add fetching demo data action

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,17 @@
-import { CHANGE_APP_LANG } from './types';
+import { CHANGE_APP_LANG, FETCH_DATA } from './types';
 
-// eslint-disable-next-line import/prefer-default-export
 export function changeAppLang(lang) {
   return {
     type: CHANGE_APP_LANG,
     payload: lang,
+  };
+}
+
+export function fetchData() {
+  return async (dispatch) => {
+    // eslint-disable-next-line no-undef
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=10');
+    const json = await response.json();
+    dispatch({ type: FETCH_DATA, payload: json });
   };
 }
