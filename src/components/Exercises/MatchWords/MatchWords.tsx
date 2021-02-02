63093dbe0896683c413793b7c1985168d334e4d0
Irina Stepanova <irina.s.vesna@mail.ru>
2021-02-02 22:21:54 +0600

    feat: add sound state to match-words exercise

--- src/components/Exercises/MatchWords/MatchWords.tsx
+++ src/components/Exercises/MatchWords/MatchWords.tsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
 import './matchWords.css';
 import { Button } from 'antd';
 import { matchWordsConst, nextButtonConst } from '../../../assets/appLangConst';
 
-type Props={
-  words:string[][],
-  progress:number,
-  setProgress:React.Dispatch<React.SetStateAction<number>>,
-  points:number,
-  setPoints:React.Dispatch<React.SetStateAction<number>>,
-  id:number,
-  visibleID:number,
-  setVisibleID:React.Dispatch<React.SetStateAction<number>>,
-  lessonPlan:number[],
-  currentStep:number,
-  setCurrentStep:React.Dispatch<React.SetStateAction<number>>,
-  modalVisible:boolean,
-  appLang:string,
-  progressGap:number,
+type Props = {
+  words: string[][],
+  progress: number,
+  setProgress: React.Dispatch<React.SetStateAction<number>>,
+  points: number,
+  setPoints: React.Dispatch<React.SetStateAction<number>>,
+  id: number,
+  visibleID: number,
+  setVisibleID: React.Dispatch<React.SetStateAction<number>>,
+  lessonPlan: number[],
+  currentStep: number,
+  setCurrentStep: React.Dispatch<React.SetStateAction<number>>,
+  modalVisible: boolean,
+  appLang: string,
+  progressGap: number,
 };
 
 const MatchWords = ({
   words, progress, setProgress, points, setPoints, id, visibleID, setVisibleID,
   lessonPlan, currentStep, setCurrentStep, modalVisible, appLang, progressGap,
-}:Props) => {
+}: Props) => {
   function shuffle(array: any) {
     const shuffled = array.slice();
     for (let i = shuffled.length - 1; i > 0; i -= 1) {
@@ -45,26 +46,31 @@ const MatchWords = ({
   const [prev, setPrev]: [any, any] = useState(null);
   const [isDisabled, setDisabled]: [any, any] = useState(true);
   const [pointsPenalty, setPointsPenalty] = useState(0);
-  const wordsContainerRef:any = useRef(null);
-  const nextButtonRef:any = useRef(null);
-  const audioRef:any = useRef(null);
+  const wordsContainerRef: any = useRef(null);
+  const nextButtonRef: any = useRef(null);
+  const audioRef: any = useRef(null);
 
-  const showNewWords = (step:number) => {
+  const showNewWords = (step: number) => {
     const MAX_PROGRESS = 100;
     if (progress === MAX_PROGRESS) {
       const parent = wordsContainerRef.current.parentNode;
-      while (parent!.firstChild) { parent!.firstChild.remove(); }
+      while (parent!.firstChild) {
+        parent!.firstChild.remove();
+      }
     } else {
-      Array.from(wordsContainerRef.current.children).forEach((button:any) => {
-        const theButton:HTMLButtonElement = button;
+      Array.from(wordsContainerRef.current.children).forEach((button: any) => {
+        const theButton: HTMLButtonElement = button;
         theButton.classList.remove('match-words__word--picked');
         theButton.classList.remove('match-words__word--animate');
         theButton.disabled = false;
       });
+
       setDisabled(true);
       nextButtonRef.current.classList.add('match-word__next-button--hidden');
+
       currentArray = shuffle(shuffle(words).slice(0, NUMBER_OF_WORDS).flat());
       setCurrentWords(currentArray);
+
       setPicked(new Set(''));
       setPointsPenalty(0);
       setVisibleID(lessonPlan[step + 1]);
@@ -89,7 +95,7 @@ const MatchWords = ({
   const modalRef = useRef(modalVisible);
   modalRef.current = modalVisible;
 
-  const handleEnterPress = (event:any) => {
+  const handleEnterPress = (event: any) => {
     if (visRef.current === id
       && event.key === 'Enter'
       && nextButtonRef.current
@@ -106,7 +112,13 @@ const MatchWords = ({
     };
   }, []);
 
+  const selectSoundState = (state: { app: { isSoundOn: any; }; }) => state.app.isSoundOn;
+  const isSoundOn = useSelector(selectSoundState);
+
   function playSound(url: string) {
+    if (!isSoundOn) {
+      return;
+    }
     audioRef.current.src = url;
     audioRef.current.play();
   }
@@ -120,25 +132,30 @@ const MatchWords = ({
       button.disabled = true;
       setPrev(evt);
     } else {
-      const wordPair:any = words.find((pair: string[]) => pair.includes(prev.target.innerText));
+      const wordPair: any = words.find((pair: string[]) => pair.includes(prev.target.innerText));
       if (wordPair.includes(button.innerText)) {
         const CORRECT_URL = '../../../audio/success_sound.mp3';
         playSound(CORRECT_URL);
+
         button.classList.add('match-words__word--picked');
         button.classList.add('match-words__word--animate');
         prev.target.classList.add('match-words__word--animate');
         prev.target.disabled = true;
+
         setPicked(() => picked.add(button.innerText));
         setPrev(null);
         button.disabled = true;
       } else {
         const PENALTY_PER_MISS = 2;
         setPointsPenalty(pointsPenalty + PENALTY_PER_MISS);
+
         const ERROR_URL = '../../../audio/mistake_sound.mp3';
         playSound(ERROR_URL);
+
         prev.target.classList.remove('match-words__word--picked');
         prev.target.classList.add('match-words__word--wrong');
         button.classList.add('match-words__word--wrong');
+
         setTimeout(() => {
           prev.target.classList.remove('match-words__word--wrong');
           button.classList.remove('match-words__word--wrong');
