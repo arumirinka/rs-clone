ed7a4a05180a08b1b79a7367e38f340e36a0767d
Ekaterina <fedotova490@gmail.com>
2021-02-03 18:01:21 +0300

    feat: add media queries for loginPage

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import BlaBlaImg from '../../components/LanguagesLayout/BlaBlaImg';
 import KittyImg from '../../components/LessonsLayout/KittyImg';
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm';
+import './loginPage.css';
 
 const LoginPage: React.FC = () => (
   <>
     <div className="lang-container">
-      <div className="lang-container__inner-container1">
-        <div className="inner-container1__images">
-          <div className="images__logo">CATalie wanna speak</div>
-          <div className="images__svg">
+      <div className="lang-container__inner-container1 login-container__inner-contaner1">
+        <div className="inner-container1__images inner-container1__registration-images">
+          <div className="images__logo registration__logo">CATalie wanna speak</div>
+          <div className="images__svg registration__svg">
             <KittyImg />
             <BlaBlaImg />
           </div>
