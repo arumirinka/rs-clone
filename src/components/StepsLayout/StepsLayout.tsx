c247bbdc138bbdc731bcfbdc7b7ff08aab9f92e9
Ekaterina <fedotova490@gmail.com>
2021-02-03 20:50:29 +0300

    feat: add locks to steps

--- src/components/StepsLayout/StepsLayout.tsx
+++ src/components/StepsLayout/StepsLayout.tsx
@@ -84,36 +84,66 @@ const StepsLayout: React.FC = () => {
     maxWidth: '1200px',
   };
 
+  const levelOpenStyle: CSS.Properties = {
+    background: '#1C8673',
+    filter: 'none',
+  };
   return (
     <div style={styles} ref={refContainer}>
       <img src={kittyImg} alt="super kitty" className="kitty-img anim-11" style={kittyPosition} />
       <div className="steps-сontainer">
         <div className="steps-container__step">
           <img src={arrowLogo} alt="arrow right" className="step__arrow-right anim-2" />
-          <button type="button" className="step__level anim-1" data-id="1" onClick={(event: React.MouseEvent) => setLessonsPage(event, 1)}>
+          <button
+            type="button"
+            className="step__level anim-1"
+            style={levelOpenStyle}
+            data-id="1"
+            onClick={(event: React.MouseEvent) => setLessonsPage(event, 1)}
+          >
             {lessonsConst[appLang].level} 1
           </button>
         </div>
         <div className="steps-container__step">
           <img src={arrowLogo} alt="arrow right" className="step__arrow-right anim-4" />
-          <button type="button" className="step__level anim-3" data-id="2" onClick={(event: React.MouseEvent) => setLessonsPage(event, 2)}>
+          <button
+            type="button"
+            className="step__level anim-3 step__level--closed"
+            data-id="2"
+            onClick={(event: React.MouseEvent) => setLessonsPage(event, 2)}
+          >
             {lessonsConst[appLang].level} 2
           </button>
         </div>
         <div className="steps-container__step">
           <img src={arrowLogo} alt="arrow right" className="step__arrow-right anim-6" />
-          <button type="button" className="step__level anim-5" data-id="3" onClick={(event: React.MouseEvent) => setLessonsPage(event, 3)}>
+          <button
+            type="button"
+            className="step__level anim-5 step__level--closed"
+            data-id="3"
+            onClick={(event: React.MouseEvent) => setLessonsPage(event, 3)}
+          >
             {lessonsConst[appLang].level} 3
           </button>
         </div>
         <div className="steps-container__step">
           <img src={arrowLogo} alt="arrow right" className="step__arrow-right anim-8" />
-          <button type="button" className="step__level anim-7" data-id="4" onClick={(event: React.MouseEvent) => setLessonsPage(event, 4)}>
+          <button
+            type="button"
+            className="step__level anim-7 step__level--closed"
+            data-id="4"
+            onClick={(event: React.MouseEvent) => setLessonsPage(event, 4)}
+          >
             {lessonsConst[appLang].level} 4
           </button>
         </div>
         <div className="steps-container__step anim-10">
-          <button type="button" className="step__level anim-9" data-id="5" onClick={(event: React.MouseEvent) => setLessonsPage(event, 5)}>
+          <button
+            type="button"
+            className="step__level anim-9 step__level--closed"
+            data-id="5"
+            onClick={(event: React.MouseEvent) => setLessonsPage(event, 5)}
+          >
             {lessonsConst[appLang].level} 5
           </button>
         </div>
